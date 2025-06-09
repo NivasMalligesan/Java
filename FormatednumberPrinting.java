442d8f703d443b78f532dc3679c662f3013d719c
Nivas Malligesan <nivasmalligesan23@gmail.com>
2025-06-09 14:49:49 +0530

    Create FormatednumberPrinting.java

---
+++ FormatednumberPrinting.java
@@ -0,0 +1,14 @@
+package Training;
+import java.util.*;
+public class FormatednumberPrinting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int number = scan.nextInt();
+		System.out.printf("%05d",number);
+		
+	}
+
+}
