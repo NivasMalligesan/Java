795524ff460ed884e2a920d6bebfa09b5b1d3d1a
Nivas Malligesan <nivasmalligesan23@gmail.com>
2025-06-20 14:34:17 +0530

    Create toUpperCase.java

---
+++ toUpperCase.java
@@ -0,0 +1,12 @@
+//7.Create a new string containing only UPPERCASE from given string
+
+import java.util.*;
+
+public class Main{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+        System.out.println("String Upper : " + string.toUpperCase());
+    }
+    
+} 
