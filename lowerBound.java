1e5ca1b61a839540850b0e5863243efde2cba3c9
Nivas Malligesan <nivasmalligesan23@gmail.com>
2025-12-05 10:53:51 +0530

    Create lowerBound.java

---
+++ lowerBound.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = new int[]{1,2,3,3,5,5,5,8};
+	    int start = 0;
+	    int end = arr.length-1;
+	    int target = 5;
+	    int ans = arr.length-1;
+	    
+	    while(start <= end){
+	        int mid = start + (end - start)/2;
+	        if(arr[mid] >= target){
+	            end = mid-1;
+	            ans = mid;
+	        }else{
+	            start = mid+1;
+	        }
+	    }
+	    
+	    System.out.print(ans);
+	}
+}
